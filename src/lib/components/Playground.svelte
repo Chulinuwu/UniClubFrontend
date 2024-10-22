59c52786e8240f6206f0842e73b7335ffa76c25f
Chulinuwu <ananzxzoxd@gmail.com>
2024-10-23 03:11:11 +0700

    initial playground

--- src/lib/components/Playground.svelte
+++ src/lib/components/Playground.svelte
@@ -1,11 +1,12 @@
 <script type="ts">
-
+    import uniclub from "$lib/images/Uniclub.png"
 </script>
 
 
 <section class="p-10">
-    <div class="font-semibold text-xl text-uni-red">
+
+    <div class="w-full justify-center flex h-[100px]">    <img src={uniclub} alt="uniclub" /></div>
+    <div class="font-semibold text-xl mt-10 w-full flex justify-center text-uni-red">
         PlayGround (CEDT Sandbox)
     </div>
-
 </section>
