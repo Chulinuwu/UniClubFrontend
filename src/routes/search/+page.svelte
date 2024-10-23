f29c642372bf5774062a68d2768264408ad603d3
Chulinuwu <ananzxzoxd@gmail.com>
2024-10-23 19:21:42 +0700

    search filter function

--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -1,92 +1,65 @@
-<script type="ts">
+<script lang="ts">
     import Topnavbar from "$lib/components/Topnavbar.svelte";
     import BottomNavbar from "$lib/components/bottomNavbar.svelte";
-	import FeedCard from "$lib/components/feedCard.svelte";
-    import profile2 from "$lib/images/profile2.jpg";
-    import profile3 from "$lib/images/profile3.jpg";
-    import profile4 from "$lib/images/profile4.jpg";
+    import FeedCard from "$lib/components/feedCard.svelte";
+    import { clubMemos as memos } from '$lib/mock/clubmemos';
+    import SearchBar from '$lib/components/searchBar.svelte';
+    import TabCapsule from '$lib/components/tabCapsule.svelte';
 
-    const memos = [
-        {
-            date: '10/22/2024',
-            imageURL: profile2,
-            name: 'CEDT Sandbox Clubbbb',
-            header: 'Moodeng is born i am very happy to see Moodeng'
-        },
-        {
-            date: '11/15/2024',
-            imageURL: profile2,
-            name: 'Music Club',
-            header: 'We had an amazing concert last night!'
-        },
-        {
-            date: '12/01/2024',
-            imageURL: profile2,
-            name: 'Art Club',
-            header: 'Our art exhibition was a great success!'
-        },
-        {
-            date: '01/10/2025',
-            imageURL: profile2,
-            name: 'Science Club',
-            header: 'Our science fair showcased incredible projects!'
-        },
-        {
-            date: '02/20/2025',
-            imageURL: profile2,
-            name: 'Drama Club',
-            header: 'The drama performance was a hit!'
-        },
-        {
-            date: '03/05/2025',
-            imageURL: profile2,
-            name: 'Literature Club',
-            header: 'We had a wonderful book reading session.'
-        },
-        {
-            date: '04/12/2025',
-            imageURL: profile2,
-            name: 'Sports Club',
-            header: 'Our team won the inter-college championship!'
-        },
-        {
-            date: '05/18/2025',
-            imageURL: profile2,
-            name: 'Photography Club',
-            header: 'Our photo walk captured stunning images.'
-        },
-        {
-            date: '06/25/2025',
-            imageURL: profile2,
-            name: 'Cooking Club',
-            header: 'We had a fun and delicious cooking competition.'
-        },
-        {
-            date: '07/30/2025',
-            imageURL: profile2,
-            name: 'Robotics Club',
-            header: 'Our robots performed exceptionally well in the competition.'
-        }
+    let searchValue = '';
+    let selectedTabs: string[] = [];
+    let tabs = [
+        { label: 'Science', active: false },
+        { label: 'Arts', active: false },
+        { label: 'Sports', active: false }
     ];
 
+    function toggle(index: number) {
+        tabs = tabs.map((tab, i) => (i === index ? { ...tab, active: !tab.active } : tab));
+        const tab = tabs[index];
+        selectedTabs = tab.active
+            ? [...selectedTabs, tab.label]
+            : selectedTabs.filter((label) => label !== tab.label);
+    }
+
+    $: filteredMemos = memos.filter((memo) => {
+        const matchesName = memo.name.toLowerCase().includes(searchValue.toLowerCase());
+        const matchesCategory = selectedTabs.length > 0 ? selectedTabs.includes(memo.tags.category) : true;
+        return matchesName && matchesCategory;
+    });
+
+   
 </script>
 
 <div class="w-full h-full">
-    <Topnavbar/>
-    
-    <div class="w-full h-[300px] bg-red-300 flex justify-center items-center text-center">
-        SEARCH
-    </div>
-<div class="p-5 gap-[10px] flex flex-col w-full items-center mb-[100px]">
-    {#each memos as memo}
-    <FeedCard
-        date={memo.date}
-        imageURL={memo.imageURL}
-        name={memo.name}
-        header={memo.header}
-    />
-{/each}
 
-</div>
+
+    <div class="p-4">
+        <SearchBar bind:value={searchValue} />
+        <div class="text-uni-red">Capsule Filter</div>
+        <div class="w-full h-1 bg-uni-red"></div>
+        <div class="flex flex-wrap items-center gap-2">
+            {#each tabs as tab, i}
+                <TabCapsule active={tab.active} label={tab.label} on:click={() => toggle(i)} />
+            {/each}
+        </div>
+        {#if selectedTabs.length > 0}
+            <p class="mt-4">Selected Tabs: {selectedTabs.join(', ')}</p>
+        {:else}
+            <p class="mt-4">No Tab Selected</p>
+        {/if}
+    </div>
+
+    <div class="p-5 gap-[10px] flex flex-col w-full items-center mb-[100px]">
+        {#each filteredMemos as memo}
+            <FeedCard
+                date={memo.date}
+                imageURL={memo.imageURL}
+                name={memo.name}
+                header={memo.header}
+            />
+        {/each}
+    </div>
+
     <BottomNavbar/>
 </div>
