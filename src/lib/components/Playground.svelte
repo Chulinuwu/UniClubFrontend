506231b008731eb4cd86450d9caf791a73b405d5
Chulinuwu <ananzxzoxd@gmail.com>
2024-10-23 15:16:44 +0700

    iconProfile

--- src/lib/components/Playground.svelte
+++ src/lib/components/Playground.svelte
@@ -1,16 +1,24 @@
 <script type="ts">
-	import uniclub from '$lib/images/Uniclub.png';
 	import Button from './button.svelte';
 	import Placeholder from './placeholder.svelte';
+	// import Topnavbar from './Topnavbar.svelte';
+	import ClubNavbar from './ClubNavbar.svelte';
+	// import ProfileNavbar from './profileNavbar.svelte';
+	import ProfileIcon from './profile.svelte';
+
+
 	import home from './../icon/homeicon.svg';
 	import edit from '../icon/edit.svg';
 	import storkeheart from '../icon/strokeheart.svg';
 	import fullheart from '../icon/fullheart.svg';
 	import notification from '../icon/notification.svg';
 	import search from '../icon/search.svg';
-	// import Topnavbar from './Topnavbar.svelte';
-	import ClubNavbar from './ClubNavbar.svelte';
-	// import ProfileNavbar from './profileNavbar.svelte';
+	import uniclub from '$lib/images/Uniclub.png';
+	import profile1 from '../images/profile1.png';
+	import profile2 from '../images/profile2.jpg';
+	import profile3 from '../images/profile3.jpg';
+	import profile4 from '../images/profile4.jpg';
+
 
 	let x = 0;
 	function handlebuttonclick() {
@@ -58,6 +66,17 @@
 				<img src={fullheart} alt="fullheart" />
 				<img src={notification} alt="notification" />
 				<img src={search} alt="search" />
+			</div>
+		</section>
+
+		<section class="text-2xl mt-10 flex flex-col gap-6">
+			<div class="text-uni-red">Profile icon</div>
+			<div class="w-full h-1 bg-uni-red"></div>
+			<div class="flex flex-wrap gap-5 items-center">
+			<ProfileIcon imageURL={profile1} class="border-2 border-black" />
+			<ProfileIcon imageURL={profile4}  />
+			<ProfileIcon imageURL={profile2} class="border-1 w-[75px] h-[75px] border-uni-graytext" />
+			<ProfileIcon imageURL={profile3} class="border w-[50px] h-[50px] border-uni-graybox" />
 			</div>
 		</section>
 	</section>
---
+++ src/lib/components/profile.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    export let imageURL: string;
+    export let className: string = '';
+	export { className as class };
+</script>
+
+<div class="white {className} rounded-full w-[100px] h-[100px] overflow-hidden">
+    <img src={imageURL} alt="profile" class="w-full h-full object-cover"/>
+</div>
