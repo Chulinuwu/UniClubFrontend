b116213594f21517cdbd39225bf5841dc73a169c
Chulinuwu <ananzxzoxd@gmail.com>
2024-10-23 19:02:45 +0700

    init structure

--- src/lib/components/bottomNavbar.svelte
+++ src/lib/components/bottomNavbar.svelte
@@ -1,54 +1,72 @@
 <script lang="ts">
-    import { onMount } from 'svelte';
-    import Home from "../icon/homeicon.svg";
-    import Homeblack from "../icon/homeblack.svg";
-    import Search from "../icon/search.svg";
-    import searchblack from "../icon/searchblack.svg";
-    import fullheart from "../icon/fullheart.svg";
-    import strokeheartblack from "../icon/strokeheartblack.svg";
-    import Profile from "./profile.svelte";
-    import profile1 from "../images/profile1.png";
+	import { onMount } from 'svelte';
+	import Home from '../icon/homeicon.svg';
+	import Homeblack from '../icon/homeblack.svg';
+	import Search from '../icon/search.svg';
+	import searchblack from '../icon/searchblack.svg';
+	import fullheart from '../icon/fullheart.svg';
+	import strokeheartblack from '../icon/strokeheartblack.svg';
+	import Profile from './profile.svelte';
+	import profile1 from '../images/profile1.png';
 
-    let isHeartFull = false;
-    let currentPath = '';
-    let showHomeIcon = false;
-    let showSearchIcon = false;
-    let showHeartIcon = false;
-    let showProfileIcon = false;
+	let isHeartFull = false;
+	let currentPath = '';
+	let showHomeIcon = false;
+	let showSearchIcon = false;
+	let showHeartIcon = false;
+	let showProfileIcon = false;
 
-    // Use onMount to ensure this code runs only on the client side
-    onMount(() => {
-        currentPath = window.location.pathname;
-        showHomeIcon = currentPath === '/dashboard'
-        showSearchIcon = currentPath === '/search'
-        showHeartIcon = currentPath === '/favorites'
-        showProfileIcon = currentPath === '/profile'
-    });
+	// Use onMount to ensure this code runs only on the client side
+	onMount(() => {
+		currentPath = window.location.pathname;
+		showHomeIcon = currentPath === '/dashboard';
+		showSearchIcon = currentPath === '/search';
+		showHeartIcon = currentPath === '/favorites';
+		showProfileIcon = currentPath === '/profile';
+	});
 </script>
 
-<div class="w-full h-[100px] max-w-[100vw] p-5 fixed bottom-0 bg-white flex items-center justify-between">
-    <div class="flex h-[25px]">
-        {#if showHomeIcon}
-            <img src={Home} alt="home" />
-        {:else}
-            <img src={Homeblack} alt="homeblack" />
-        {/if}
-    </div>
-    <div class="flex h-[25px]">
-        {#if showSearchIcon}
-            <img src={Search} alt="search" />
-        {:else}
-            <img src={searchblack} alt="searchblack" />
-        {/if}
-    </div>
-    <div class="flex h-[25px]">
-        {#if showHeartIcon}
-            <img src={fullheart} alt="fullheart" />
-        {:else}
-            <img src={strokeheartblack} alt="strokeheart" />
-        {/if}
-    </div>
-    <div class="flex h-[25px]">
-        <Profile imageURL={profile1} class="w-auto h-auto {showProfileIcon ? 'border border-uni-red' : ''}" />
-    </div>
+<div
+	class="w-full h-[100px] max-w-[100vw] p-5 fixed bottom-0 bg-white flex items-center justify-between"
+>
+	<div class="flex h-[25px]">
+		{#if showHomeIcon}
+			<img src={Home} alt="home" class="max-w-[25px]" />
+		{:else}
+			<a href="/dashboard" class="max-w-[25px]">
+				<img src={Homeblack} alt="homeblack" />
+			</a>
+		{/if}
+	</div>
+	<div class="flex h-[25px]">
+		{#if showSearchIcon}
+			<img src={Search} alt="search" class="max-w-[25px]" />
+		{:else}
+			<a href="/search" class="max-w-[25px]"> <img src={searchblack} alt="searchblack" /></a>
+		{/if}
+	</div>
+	<div class="flex h-[25px]">
+		{#if showHeartIcon}
+			<img src={fullheart} alt="fullheart" class="max-w-[25px]" />
+		{:else}
+			<a href="/favorites" class="max-w-[25px]">
+				<img src={strokeheartblack} alt="strokeheart" />
+			</a>
+		{/if}
+	</div>
+	<div class="flex h-[25px]">
+		{#if showProfileIcon}
+			<Profile
+				imageURL={profile1}
+				class="w-auto h-auto max-w-[25px] rounded-full {showProfileIcon ? 'border border-uni-red' : ''}"
+			/>
+		{:else}
+			<a href="/profile">
+				<Profile
+					imageURL={profile1}
+					class="w-auto h-auto  max-w-[25px] rounded-full {showProfileIcon ? 'border border-uni-red' : ''}"
+				/>
+			</a>
+		{/if}
+	</div>
 </div>
---
+++ src/routes/search/+page.svelte
@@ -0,0 +1,92 @@
+<script type="ts">
+    import Topnavbar from "$lib/components/Topnavbar.svelte";
+    import BottomNavbar from "$lib/components/bottomNavbar.svelte";
+	import FeedCard from "$lib/components/feedCard.svelte";
+    import profile2 from "$lib/images/profile2.jpg";
+    import profile3 from "$lib/images/profile3.jpg";
+    import profile4 from "$lib/images/profile4.jpg";
+
+    const memos = [
+        {
+            date: '10/22/2024',
+            imageURL: profile2,
+            name: 'CEDT Sandbox Clubbbb',
+            header: 'Moodeng is born i am very happy to see Moodeng'
+        },
+        {
+            date: '11/15/2024',
+            imageURL: profile2,
+            name: 'Music Club',
+            header: 'We had an amazing concert last night!'
+        },
+        {
+            date: '12/01/2024',
+            imageURL: profile2,
+            name: 'Art Club',
+            header: 'Our art exhibition was a great success!'
+        },
+        {
+            date: '01/10/2025',
+            imageURL: profile2,
+            name: 'Science Club',
+            header: 'Our science fair showcased incredible projects!'
+        },
+        {
+            date: '02/20/2025',
+            imageURL: profile2,
+            name: 'Drama Club',
+            header: 'The drama performance was a hit!'
+        },
+        {
+            date: '03/05/2025',
+            imageURL: profile2,
+            name: 'Literature Club',
+            header: 'We had a wonderful book reading session.'
+        },
+        {
+            date: '04/12/2025',
+            imageURL: profile2,
+            name: 'Sports Club',
+            header: 'Our team won the inter-college championship!'
+        },
+        {
+            date: '05/18/2025',
+            imageURL: profile2,
+            name: 'Photography Club',
+            header: 'Our photo walk captured stunning images.'
+        },
+        {
+            date: '06/25/2025',
+            imageURL: profile2,
+            name: 'Cooking Club',
+            header: 'We had a fun and delicious cooking competition.'
+        },
+        {
+            date: '07/30/2025',
+            imageURL: profile2,
+            name: 'Robotics Club',
+            header: 'Our robots performed exceptionally well in the competition.'
+        }
+    ];
+
+</script>
+
+<div class="w-full h-full">
+    <Topnavbar/>
+    
+    <div class="w-full h-[300px] bg-red-300 flex justify-center items-center text-center">
+        SEARCH
+    </div>
+<div class="p-5 gap-[10px] flex flex-col w-full items-center mb-[100px]">
+    {#each memos as memo}
+    <FeedCard
+        date={memo.date}
+        imageURL={memo.imageURL}
+        name={memo.name}
+        header={memo.header}
+    />
+{/each}
+
+</div>
+    <BottomNavbar/>
+</div>
