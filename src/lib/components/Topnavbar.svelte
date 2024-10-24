ebed27e7ccd5a8b6e0b0921ca3b42829a7962eae
Warakorn Leepuengthum <wtlee13689@hotmail.com>
2024-10-24 23:44:36 +0700

    add notification page(demo)

--- src/lib/components/Topnavbar.svelte
+++ src/lib/components/Topnavbar.svelte
@@ -1,10 +1,25 @@
 <script lang="ts">
 	import notification from '../icon/notification.svg';
+	import strokeNotification from '../icon/strokeNotification.svg';
 	import uniclub from '../images/Uniclub.png';
 	import { goto } from '$app/navigation';
+	import { onMount } from 'svelte';
+
+	let currentPath = '';
+	let showNotification = false;
+	onMount(() => {
+		currentPath = window.location.pathname;
+		showNotification = currentPath === '/notification';
+	});
 </script>
 
 <div class="w-full h-[100px] z-50 p-5 fixed bg-white flex items-center justify-between">
 	<div class=" flex h-[50px]"><img src={uniclub} alt="uniclub" /></div>
-	<a href="/notification" class="flex h-[40px] min-h-[px] min-w-8 max-w-8 items-center justify-center"><img src={notification} alt="notification" class="min-h-[40px]"/></a>
+	{#if showNotification}
+		<img src={strokeNotification} alt="notification" class="min-h-[40px]"/>
+	{:else}
+		<a href="/notification" class="flex h-[40px] min-h-[px] min-w-8 max-w-8 items-center justify-center">
+			<img src={notification} alt="notification" class="min-h-[40px]"/>
+		</a>
+	{/if}
 </div>
---
+++ src/routes/notification/+page.svelte
@@ -0,0 +1,43 @@
+<script type="ts">
+	import Topnavbar from '$lib/components/Topnavbar.svelte';
+	import BottomNavbar from '$lib/components/bottomNavbar.svelte';
+	import FeedCard from '$lib/components/feedCard.svelte';
+	import profile2 from '$lib/images/profile2.jpg';
+	import profile3 from '$lib/images/profile3.jpg';
+	import profile4 from '$lib/images/profile4.jpg';
+    import { goto } from '$app/navigation';
+
+    function handleNavigate1() {
+		goto(`/activity/F001`);
+	}
+    function handleNavigate2() {
+		goto(`/activity/F002`);
+	}
+	
+</script>
+
+<div class="w-full h-full">
+	<Topnavbar />
+    <div>
+	<div class="p-5 gap-[10px] flex flex-col w-full items-center mb-[100px] pt-[100px]">
+            <FeedCard 
+                    feedId="F001"
+                    date="10/22/2024" 
+                    imageURL= {profile2}
+                    name="CEDT Sandbox Clubbbbb" 
+                    header="Moodeng is born i am very happy to see Moodeng"
+                    on:navigate = {handleNavigate1} 
+            />
+            <FeedCard 
+                feedId="F002"
+                date="11/15/2024" 
+                imageURL= {profile2}
+                name="Music Club" 
+                header="We had an amazing concert last night!"
+                on:navigate = {handleNavigate2}
+            />
+        </div>
+    </div>
+
+	<BottomNavbar />
+</div>
