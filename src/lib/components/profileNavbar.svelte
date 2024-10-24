8d5086a727679c0ed7f1201a3793f1ab02ced3d0
Chulinuwu <ananzxzoxd@gmail.com>
2024-10-24 15:31:52 +0700

    dasdas

--- src/lib/components/profileNavbar.svelte
+++ src/lib/components/profileNavbar.svelte
@@ -2,10 +2,11 @@
 	import back from '../icon/back.svg';
 	import uniclub from '../images/uniclub.png';
 	import notification from '../icon/notification.svg';
+	import { goto } from '$app/navigation';
 </script>
 
 <div class="w-full h-[100px] p-5 fixed z-50 bg-white flex items-center justify-between">
-	<div class="flex h-[40px]"><img src={back} alt="back" /></div>
+	<button on:click={() => history.back()} class="flex h-[40px]"><img src={back} alt="back" /></button>
 	<div class="flex h-[50px]"><img src={uniclub} alt="uniclub" /></div>
 	<div class="flex h-[40px]"><img src={notification} alt="notification" /></div>
 </div>
