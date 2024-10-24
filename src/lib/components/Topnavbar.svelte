ed1f80d0f892a6419dc46e6018802f48ecc09fdd
Chulinuwu <ananzxzoxd@gmail.com>
2024-10-24 14:33:53 +0700

    asdasd

--- src/lib/components/Topnavbar.svelte
+++ src/lib/components/Topnavbar.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
 	import notification from '../icon/notification.svg';
 	import uniclub from '../images/uniclub.png';
+	import { goto } from '$app/navigation';
 </script>
 
 <div class="w-full h-[100px] z-50 p-5 fixed bg-white flex items-center justify-between">
 	<div class=" flex h-[50px]"><img src={uniclub} alt="uniclub" /></div>
-	<div class="flex h-[40px]"><img src={notification} alt="notification" /></div>
+	<a href="/notification" class="flex h-[40px]"><img src={notification} alt="notification" /></a>
 </div>
