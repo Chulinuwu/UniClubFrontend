4ad73a8dd33cb3977f9a757214c28763af9ccf25
Chulinuwu <ananzxzoxd@gmail.com>
2024-10-24 12:26:18 +0700

    asdasd

--- src/routes/clubs/[clubId]/+page.svelte
+++ src/routes/clubs/[clubId]/+page.svelte
@@ -8,17 +8,17 @@
 <ClubNavbar />
   
   {#if club}
-    <div class="w-full h-full flex flex-col items-center justify-center">
+    <div class="w-full h-full flex flex-col gap-[10px] items-center justify-center">
         <div class="flex w-full h-[100px] bg-slate-300"></div>
         <div class="flex w-full h-[256px]">
             <img src={clubbg} alt="clubbg" class="w-full h-full object-cover" />
         </div>
         <div class="absolute top-52 w-full flex justify-center">
-        <img src={club.imageURL} alt="{club.name}" class="w-60 h-60 object-cover shadow-lg rounded-xl" />
+        <img src={club.imageURL} alt="{club.name}" class="w-60  max-w-[240px] max-h-[240px] h-60 object-cover shadow-lg rounded-xl" />
         </div>
         <div class="flex flex-col w-full h-auto text-center items-center mt-28">
-      <h1 class="text-4xl font-bold text-black">{club.name}</h1>
-      <p class="text-lg text-uni-graytext">{club.header}</p>
+      <h1 class="text-xl font-bold text-black">{club.name}</h1>
+      <p class="text-base text-uni-graytext">{club.header}</p>
       <div class="flex w-1/2 h-0.5 my-4 bg-uni-red"></div>
     </div>
     <div class="flex w-full h-[100px] bg-slate-300"></div>
