7249d52f1e3a685b61bf291390f694de2536b5e9
chulinuwuw <ananzxzoxd@gmail.com>
2024-10-25 11:45:41 +0700

    history back

--- src/lib/components/Topnavbar.svelte
+++ src/lib/components/Topnavbar.svelte
@@ -16,7 +16,9 @@
 <div class="w-full h-[100px] z-50 p-5 fixed bg-white flex items-center justify-between">
 	<div class=" flex h-[50px]"><img src={uniclub} alt="uniclub" /></div>
 	{#if showNotification}
+	<button on:click={()=>history.back()}>
 		<img src={strokeNotification} alt="notification" class="min-h-[40px]"/>
+	</button>
 	{:else}
 		<a href="/notification" class="flex h-[40px] min-h-[px] min-w-8 max-w-8 items-center justify-center">
 			<img src={notification} alt="notification" class="min-h-[40px]"/>
--- src/lib/components/profileNavbar.svelte
+++ src/lib/components/profileNavbar.svelte
@@ -3,6 +3,15 @@
 	import uniclub from '../images/Uniclub.png';
 	import notification from '../icon/notification.svg';
 	import { goto } from '$app/navigation';
+	import strokeNotification from '../icon/strokeNotification.svg';
+	import { onMount } from 'svelte';
+
+	let currentPath = '';
+	let showNotification = false;
+	onMount(() => {
+		currentPath = window.location.pathname;
+		showNotification = currentPath === '/notification';
+	});
 </script>
 
 <div class="w-full h-[100px] p-5 fixed z-50  bg-white flex items-center justify-between">
@@ -10,5 +19,13 @@
 		><img src={back} alt="back" /></button
 	>
 	<!-- <div class="flex h-[50px]"><img src={uniclub} alt="uniclub" /></div> -->
-	<div class="flex h-[40px] min-w-10"><img src={notification} alt="notification" /></div>
+	{#if showNotification}
+	<button on:click={()=>history.back()}>
+		<img src={strokeNotification} alt="notification" class="min-h-[40px]"/>
+	</button>
+	{:else}
+		<a href="/notification" class="flex h-[40px] min-h-[px] min-w-8 max-w-8 items-center justify-center">
+			<img src={notification} alt="notification" class="min-h-[40px]"/>
+		</a>
+	{/if}
 </div>
